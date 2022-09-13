98ee40821a78a42d32dc63860825194171175a22
Dorthy Rodriguez <emddr@gmail.com>
2022-09-13 12:05:33 -0700

    mgjpBranch

--- Mapping_GeoJson_Points/static/js/logic.js
+++ Mapping_GeoJson_Points/static/js/logic.js
@@ -1,79 +1,43 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
-//We create the map object with options 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  pointToLayer: function(feature, latlng) {
-    console.log(feature);
-    return L.marker(latlng)
-    .bindPopup("<h2>" + "Airport code: " + feature.properties.id + "</h2> <hr>  <h3>"  + feature.properties.name +"</h3>")
-}
-}).addTo(map);
-
-// Coordinates for each point to be used in the polyline.
-L.polyline([
-  [37.6213, -122.3790],
-  [30.1975, -97.6664],
-  [43.677, -79.6248],
-  [40.6413, -73.7781]
-], {
-  color: "blue",
-  weight: 4,
-  opacity: 0.5,
-  dashArray: '5,5',
-  
-}).addTo(map);
-
-//  Add a marker to the map for Los Angeles, California.
-// An array containing each city's location, state, and population.
-// Get data from cities.js
-
-let cityData = cities;
-
-
-// Loop through the cities array and create one marker for each city.
-//cityData.forEach(function(city) {
-    //console.log(city)
-    //L.circleMarker(city.location, {
-      //radius: city.population/100000,
-      //color:"black",
-      //fillColor:'#ffffa1'
-   // })
-    //.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-   // .addTo(map);
-   //});
-
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
+//We create a tile layer that will be an option for our maps 
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+//Create a base layer that holds both maps
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create a base map object wixth center, zoom level and default layer. 
+let map = L.map ("mapid", {
+  center:[30, 30],
+  zoom: 2,
+  layers: [streets]
+});
+
+//Pass map layers into layer control and add layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+//streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/Myorignl/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
